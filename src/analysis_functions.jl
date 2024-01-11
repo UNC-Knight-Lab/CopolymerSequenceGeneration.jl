0ae326fa7aabab51b6728b16705b0d141c3e848a
bridgepoff <146767886+bridgepoff@users.noreply.github.com>
2024-01-11 16:49:28 -0500

    update to analysis functions

--- src/analysis_functions.jl
+++ src/analysis_functions.jl
@@ -1,1 +1,14 @@
-n=1
+function KL_divergence(p, q)
+    kl = 0
+    for i in eachindex(p)
+        kl += p[i] * log2(p[i]/q[i])
+    end
+    return kl
+end    
+
+function JS_divergence(p,q)
+    p /= sum(p)
+    q /= sum(q)
+    m = (p + q) * 0.5
+    return 0.5 * KL_divergence(p, m) + 0.5 * KL_givergence(q,m)
+end

e9cbded83f340aa79d47248a9aac4a73104b0815
bridgepoff <146767886+bridgepoff@users.noreply.github.com>
2024-01-12 13:13:03 -0500

    updated analysis functions

--- src/analysis_functions.jl
+++ src/analysis_functions.jl
@@ -1,14 +1,26 @@
-function KL_divergence(p, q)
-    kl = 0
-    for i in eachindex(p)
-        kl += p[i] * log2(p[i]/q[i])
-    end
-    return kl
-end    
+using Pkg
+Pkg.add("CSV")
 
-function JS_divergence(p,q)
-    p /= sum(p)
-    q /= sum(q)
-    m = (p + q) * 0.5
-    return 0.5 * KL_divergence(p, m) + 0.5 * KL_givergence(q,m)
-end
+function Replace(file_path)
+    p = CSV.read(file_path) |> Matrix
+    println(p)
+
+p = CSV.read("sample code/sample data/triblock-sequences.csv")
+println(p)
+
+# function KL_divergence(p, q)
+#     kl = 0
+#     for i in eachindex(p)
+#         kl += p[i] * log2(p[i]/q[i])
+#     end
+#     return kl
+# end    
+
+# function JS_divergence(p,q)
+#     p /= sum(p)
+#     q /= sum(q)
+#     m = (p + q) * 0.5
+#     return 0.5 * KL_divergence(p, m) + 0.5 * KL_givergence(q,m)
+# end
+
+# Replace("sample code/sample data/triblock-sequences.csv")
